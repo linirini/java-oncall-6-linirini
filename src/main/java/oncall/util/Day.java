90a5514b884d4adf7738fdd99f95d3ff16af1988
linirini <2001yerin@naver.com>
2023-12-16 15:29:39 +0900

    test : test for checking if it is weekend or week

--- src/main/java/oncall/util/Day.java
+++ src/main/java/oncall/util/Day.java
@@ -1,23 +1,25 @@
 package oncall.util;
 
+import static oncall.util.ExceptionEnum.INVALID_DAY;
+
 import java.util.Arrays;
 
 public enum Day {
 
-    MONDAY("월",1),
+    MONDAY("월", 1),
 
-    TUESDAY("화",2),
+    TUESDAY("화", 2),
 
-    WEDNESDAY("수",3),
+    WEDNESDAY("수", 3),
 
-    THURSDAY("목",4),
+    THURSDAY("목", 4),
 
-    FRIDAY("금",5),
+    FRIDAY("금", 5),
 
-    SATURDAY("토",6),
+    SATURDAY("토", 6),
 
-    SUNDAY("일",7);
-    
+    SUNDAY("일", 7);
+
     private final String name;
     private final int sequence;
 
@@ -26,8 +28,13 @@ public enum Day {
         this.sequence = sequence;
     }
 
-    public static Day getDayByName(String name){
-        Arrays.stream(values()).filter(day -> day.name.equals(name)).findFirst().orElseThrow(IllegalArgumentException(INVALID_DAY.getMessage()));
+    public static Day getDayByName(String name) {
+        return Arrays.stream(values()).filter(day -> day.name.equals(name)).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_DAY.getMessage()));
+    }
+
+    public int getDaySequence(){
+        return sequence;
     }
 
 }
---
+++ src/test/java/oncall/domain/CalenderTest.java
@@ -0,0 +1,21 @@
+package oncall.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import oncall.util.Day;
+import oncall.util.Month;
+import org.junit.jupiter.api.Test;
+
+class CalenderTest {
+
+    @Test
+    void 주말이다() {
+        assertThat(new Calender(Month.DECEMBER, Day.WEDNESDAY).isWeekend(5)).isTrue();
+    }
+
+    @Test
+    void 평일이다() {
+        assertThat(new Calender(Month.DECEMBER, Day.WEDNESDAY).isWeekend(6)).isFalse();
+    }
+
+}
