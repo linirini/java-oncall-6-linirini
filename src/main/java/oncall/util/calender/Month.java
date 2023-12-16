e8d7b4dc112c4489969b374c8ea28a1719073ee4
linirini <2001yerin@naver.com>
2023-12-16 16:25:42 +0900

    fix : fix month enum;s month information into String type

--- src/main/java/oncall/util/calender/Month.java
+++ src/main/java/oncall/util/calender/Month.java
@@ -6,29 +6,29 @@ import java.util.Arrays;
 
 public enum Month {
 
-    JANUARY(1,31),
-    FEBRUARY(2,28),
-    MARCH(3,31),
-    APRIL(4,30),
-    MAY(5,31),
-    JUNE(6,30),
-    JULY(7,31),
-    AUGUST(8,31),
-    SEPTEMBER(9,30),
-    OCTOBER(10,31),
-    NOVEMBER(11,30),
-    DECEMBER(12,31);
+    JANUARY("1",31),
+    FEBRUARY("2",28),
+    MARCH("3",31),
+    APRIL("4",30),
+    MAY("5",31),
+    JUNE("6",30),
+    JULY("7",31),
+    AUGUST("8",31),
+    SEPTEMBER("9",30),
+    OCTOBER("10",31),
+    NOVEMBER("11",30),
+    DECEMBER("12",31);
 
-    private final int month;
+    private final String month;
     private final int lastDate;
 
-    Month(int month, int lastDate) {
+    Month(String month, int lastDate) {
         this.month = month;
         this.lastDate = lastDate;
     }
 
-    public static Month getMonth(int month) {
-        return Arrays.stream(values()).filter(monthEnum -> month == monthEnum.month).findFirst()
+    public static Month getMonth(String month) {
+        return Arrays.stream(values()).filter(monthEnum -> monthEnum.equals(month)).findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_MONTH.getMessage()));
     }
 
