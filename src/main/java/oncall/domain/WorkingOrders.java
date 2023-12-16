19952cd173e1aa991f6cb32adbf7669425cc522e
linirini <2001yerin@naver.com>
2023-12-16 14:26:14 +0900

    feat : implement checking if duplicated name exists

--- src/main/java/oncall/domain/WorkingOrders.java
+++ src/main/java/oncall/domain/WorkingOrders.java
@@ -1,10 +1,10 @@
 package oncall.domain;
 
-import static java.util.stream.Collectors.toList;
 import static oncall.util.Constants.MAXIMUM_NICK_NAME_COUNT;
 import static oncall.util.Constants.MAXIMUM_NICK_NAME_LENGTH;
 import static oncall.util.Constants.MINIMUM_NICK_NAME_COUNT;
 import static oncall.util.Constants.MINIMUM_NICK_NAME_LENGTH;
+import static oncall.util.ExceptionEnum.DUPLICATED_NICK_NAME;
 import static oncall.util.ExceptionEnum.INVALID_NICK_NAME_COUNT;
 import static oncall.util.ExceptionEnum.INVALID_NICK_NAME_LENGTH;
 
@@ -22,18 +22,26 @@ public class WorkingOrders {
     private void validate(List<String> workingOrders) {
         throwIfInvalidNicknameCount(workingOrders);
         throwIfInvalidNickname(workingOrders);
+        throwIfDuplicatedNickname(workingOrders);
     }
 
     private void throwIfInvalidNicknameCount(List<String> workingOrders) {
-        if(workingOrders.size()<MINIMUM_NICK_NAME_COUNT||workingOrders.size()>MAXIMUM_NICK_NAME_COUNT){
+        if (workingOrders.size() < MINIMUM_NICK_NAME_COUNT
+                || workingOrders.size() > MAXIMUM_NICK_NAME_COUNT) {
             throw new IllegalArgumentException(INVALID_NICK_NAME_COUNT.getMessage());
         }
     }
 
     private void throwIfInvalidNickname(List<String> workingOrders) {
-        if(workingOrders.stream().anyMatch(nickName -> nickName.length() < MINIMUM_NICK_NAME_LENGTH
-                || nickName.length() > MAXIMUM_NICK_NAME_LENGTH)){
+        if (workingOrders.stream().anyMatch(nickName -> nickName.length() < MINIMUM_NICK_NAME_LENGTH
+                || nickName.length() > MAXIMUM_NICK_NAME_LENGTH)) {
             throw new IllegalArgumentException(INVALID_NICK_NAME_LENGTH.getMessage());
+        }
+    }
+
+    private void throwIfDuplicatedNickname(List<String> workingOrders) {
+        if (workingOrders.size() != workingOrders.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATED_NICK_NAME.getMessage());
         }
     }
 
--- src/main/java/oncall/util/ExceptionEnum.java
+++ src/main/java/oncall/util/ExceptionEnum.java
@@ -3,7 +3,8 @@ package oncall.util;
 public enum ExceptionEnum {
 
     INVALID_NICK_NAME_LENGTH("닉네임은 최소 1자, 최대 5자 입력 가능합니다."),
-    INVALID_NICK_NAME_COUNT("최소 4명, 최대 35명의 근무자를 입력해주세요.");
+    INVALID_NICK_NAME_COUNT("최소 4명, 최대 35명의 근무자를 입력해주세요."),
+    DUPLICATED_NICK_NAME("중복된 닉네임을 입력할 수 없습니다.");
 
     private static final String ERROR = "[ERROR] ";
     private String message;
