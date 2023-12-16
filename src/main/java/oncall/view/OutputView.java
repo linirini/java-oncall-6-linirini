9959343e3e0c3f6b395bfe39bbd0e9e71f530a1d
linirini <2001yerin@naver.com>
2023-12-16 17:13:27 +0900

    feat : implement printing schedule result

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,11 +1,37 @@
 package oncall.view;
 
+import static oncall.util.Constants.BLANK;
+import static oncall.util.Constants.START_DATE;
+
+import oncall.domain.Calender;
 import oncall.domain.WorkSchedule;
+import oncall.util.calender.Holiday;
 
 public class OutputView {
 
-    public void printEmergencyWorkSchedule(WorkSchedule workSchedule){
+    private static final String MONTH = "월 ";
+    private static final String DATE = "일";
+    private static final String HOLIDAY = "(휴일)";
 
+    public void printEmergencyWorkSchedule(WorkSchedule workSchedule) {
+        Calender calender = workSchedule.getCalender();
+        for (int date = START_DATE; date <= calender.getLastDateOfMonth(); date++) {
+            StringBuilder output = getScheduleOutput(workSchedule, calender, date);
+            System.out.println(output);
+        }
+    }
+
+    private StringBuilder getScheduleOutput(WorkSchedule workSchedule, Calender calender,
+            int date) {
+        StringBuilder output = new StringBuilder();
+        output.append(calender.getMonth() + MONTH);
+        output.append(date + DATE);
+        if (!calender.isWeekend(date) && Holiday.isHoliday(calender.getMonth(), date)) {
+            output.append(HOLIDAY);
+        }
+        output.append(BLANK);
+        output.append(workSchedule.getWorker(date));
+        return output;
     }
 
 }
