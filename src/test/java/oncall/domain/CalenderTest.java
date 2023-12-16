4e0edd5ea61c00a7e74779fec1dee2e3b7b0b0c9
linirini <2001yerin@naver.com>
2023-12-16 17:38:00 +0900

    test : test for checking if the date is weekend

--- src/test/java/oncall/domain/CalenderTest.java
+++ src/test/java/oncall/domain/CalenderTest.java
@@ -4,15 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import oncall.util.calender.Day;
 import oncall.util.calender.Month;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalenderTest {
 
-    @Test
-    void 주말이다() {
-        assertThat(new Calender(Month.DECEMBER, Day.WEDNESDAY).isWeekend(5)).isTrue();
+    @ParameterizedTest
+    @ValueSource(ints={1,2,8,9,15,16,22,23,29,30})
+    void 주말이다(int date) {
+        assertThat(new Calender(Month.APRIL, Day.SATURDAY).isWeekend(date)).isTrue();
     }
 
     @Test
