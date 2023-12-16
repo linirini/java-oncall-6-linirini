5bdb5006b5f149a933ba879eda6d31d88ea7314c
linirini <2001yerin@naver.com>
2023-12-16 14:45:33 +0900

    feat : implement Calender Enum

--- src/main/java/oncall/util/Calender.java
+++ src/main/java/oncall/util/Calender.java
@@ -1,7 +1,33 @@
 package oncall.util;
 
+import static oncall.util.ExceptionEnum.INVALID_MONTH;
+
+import java.util.Arrays;
+
 public enum Calender {
 
-    ;
+    JANUARY(1),
+    FEBRUARY(2),
+    MARCH(3),
+    APRIL(4),
+    MAY(5),
+    JUNE(6),
+    JULY(7),
+    AUGUST(8),
+    SEPTEMBER(9),
+    OCTOBER(10),
+    NOVEMBER(11),
+    DECEMBER(12);
+
+    private final int month;
+
+    Calender(int month) {
+        this.month = month;
+    }
+
+    public static Calender getMonth(int month) {
+        return Arrays.stream(values()).filter(monthEnum -> month == monthEnum.month).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_MONTH.getMessage()));
+    }
 
 }
