a93d550407ff2e01b6cd878966cbc3a4391c37fa
linirini <2001yerin@naver.com>
2023-12-16 14:21:19 +0900

    test : test for checking exception thrown when name is longer than 5

---
+++ src/test/java/oncall/domain/WorkingOrdersTest.java
@@ -0,0 +1,18 @@
+package oncall.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WorkingOrdersTest {
+
+    @DisplayName("5자 이상인 닉네임이 존재하면 예외를 던진다.")
+    @Test
+    void 이름_길이_예외() {
+        assertThatThrownBy(() -> new WorkingOrders(List.of("가나", "가나다라마바"))).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+}
