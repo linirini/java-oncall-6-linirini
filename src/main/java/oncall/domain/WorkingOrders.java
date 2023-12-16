802412920b75411c90907bdf7191e482000e7c3a
linirini <2001yerin@naver.com>
2023-12-16 14:24:05 +0900

    feat : implement checking if worker is less than 5, more than 35

--- src/main/java/oncall/domain/WorkingOrders.java
+++ src/main/java/oncall/domain/WorkingOrders.java
@@ -1,8 +1,11 @@
 package oncall.domain;
 
 import static java.util.stream.Collectors.toList;
+import static oncall.util.Constants.MAXIMUM_NICK_NAME_COUNT;
 import static oncall.util.Constants.MAXIMUM_NICK_NAME_LENGTH;
+import static oncall.util.Constants.MINIMUM_NICK_NAME_COUNT;
 import static oncall.util.Constants.MINIMUM_NICK_NAME_LENGTH;
+import static oncall.util.ExceptionEnum.INVALID_NICK_NAME_COUNT;
 import static oncall.util.ExceptionEnum.INVALID_NICK_NAME_LENGTH;
 
 import java.util.List;
@@ -17,7 +20,14 @@ public class WorkingOrders {
     }
 
     private void validate(List<String> workingOrders) {
+        throwIfInvalidNicknameCount(workingOrders);
         throwIfInvalidNickname(workingOrders);
+    }
+
+    private void throwIfInvalidNicknameCount(List<String> workingOrders) {
+        if(workingOrders.size()<MINIMUM_NICK_NAME_COUNT||workingOrders.size()>MAXIMUM_NICK_NAME_COUNT){
+            throw new IllegalArgumentException(INVALID_NICK_NAME_COUNT.getMessage());
+        }
     }
 
     private void throwIfInvalidNickname(List<String> workingOrders) {
--- src/main/java/oncall/util/ExceptionEnum.java
+++ src/main/java/oncall/util/ExceptionEnum.java
@@ -2,7 +2,8 @@ package oncall.util;
 
 public enum ExceptionEnum {
 
-    INVALID_NICK_NAME_LENGTH("닉네임은 최소 1자, 최대 5자 입력 가능합니다.");
+    INVALID_NICK_NAME_LENGTH("닉네임은 최소 1자, 최대 5자 입력 가능합니다."),
+    INVALID_NICK_NAME_COUNT("최소 4명, 최대 35명의 근무자를 입력해주세요.");
 
     private static final String ERROR = "[ERROR] ";
     private String message;
