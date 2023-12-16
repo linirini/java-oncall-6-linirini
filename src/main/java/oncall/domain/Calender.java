5f2e1b3b1f48f4b0b1c6dfb1ef068b8691d87ef2
linirini <2001yerin@naver.com>
2023-12-16 15:27:12 +0900

    feat : implement checking if the date is weekend

--- src/main/java/oncall/domain/Calender.java
+++ src/main/java/oncall/domain/Calender.java
@@ -1,15 +1,41 @@
 package oncall.domain;
 
+import static oncall.util.Day.SATURDAY;
+import static oncall.util.Day.SUNDAY;
+
+import java.util.List;
+import oncall.util.Day;
 import oncall.util.Month;
 
 public class Calender {
 
+    private static final int FIRST_DAY = 1;
     private final Month month;
-    private final int startDate;
+    private final Day startDay;
+    private final List<Integer> firstWeekend;
 
-    public Calender(Month month, int startDate) {
+    public Calender(Month month, Day startDay) {
         this.month = month;
-        this.startDate = startDate;
+        this.startDay = startDay;
+        firstWeekend = getFirstWeekend(startDay);
+    }
+
+    private List<Integer> getFirstWeekend(Day startDay) {
+        return List.of(getFirstSaturday(startDay), getFirstSunday(startDay));
+    }
+
+    private int getFirstSaturday(Day startDay) {
+        return FIRST_DAY + Math.abs(startDay.getDaySequence() - SATURDAY.getDaySequence());
+    }
+
+    private static int getFirstSunday(Day startDay) {
+        return FIRST_DAY + Math.abs(startDay.getDaySequence() - SUNDAY.getDaySequence());
+    }
+
+    public boolean isWeekend(int date) {
+        return (date - this.firstWeekend.get(0)) % 7 == 0 || (date - this.firstWeekend.get(1) % 7
+                == 0);
+
     }
 
 }
