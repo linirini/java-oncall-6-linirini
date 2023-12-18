03438c8f76c5d5b98f7167bc06071fb1f401060e
linirini <2001yerin@naver.com>
2023-12-18 23:59:16 +0900

    fix : checking if working orders list is equal

--- src/main/java/oncall/domain/Orders.java
+++ src/main/java/oncall/domain/Orders.java
@@ -4,15 +4,19 @@ import static oncall.util.Constants.WEEKEND_ORDER;
 import static oncall.util.Constants.WEEK_ORDER;
 import static oncall.util.ExceptionEnum.SAME_ORDERS;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Orders {
 
     List<WorkingOrders> orders;
 
-    public Orders(List<WorkingOrders> orders) {
-        validate(orders);
-        this.orders = orders;
+    public Orders(WorkingOrders weekWorkingOrders, WorkingOrders weekendWorkingOrders) {
+        List<WorkingOrders> workingOrders = new ArrayList<>();
+        workingOrders.add(WEEK_ORDER,weekWorkingOrders);
+        workingOrders.add(WEEKEND_ORDER,weekendWorkingOrders);
+        validate(workingOrders);
+        this.orders = workingOrders;
     }
 
     private void validate(List<WorkingOrders> orders) {
@@ -20,7 +24,7 @@ public class Orders {
     }
 
     private void throwIfSameOrders(List<WorkingOrders> orders) {
-        if(getWeekOrders().equals(getWeekendOrders())){
+        if(orders.get(WEEK_ORDER).getWorkingOrders().equals(orders.get(WEEKEND_ORDER).getWorkingOrders())){
             throw new IllegalArgumentException(SAME_ORDERS.getMessage());
         }
     }
