4e2555ce352a553e89e614b176c93debb9c419ef
linirini <2001yerin@naver.com>
2023-12-19 00:06:46 +0900

    refactor : change allocate methods arguments

--- src/main/java/oncall/service/AllocationService.java
+++ src/main/java/oncall/service/AllocationService.java
@@ -5,20 +5,16 @@ import static oncall.util.Constants.START_DATE;
 import oncall.domain.Calender;
 import oncall.domain.Orders;
 import oncall.domain.WorkSchedule;
-import oncall.domain.WorkingOrders;
 import oncall.util.calender.Holiday;
 
 public class AllocationService {
 
     public WorkSchedule allocate(Calender calender, Orders orders) {
         WorkSchedule workSchedule = new WorkSchedule(calender);
-        WorkingOrders weekWorkers = orders.getWeekOrders();
-        WorkingOrders weekendWorkers = orders.getWeekendOrders();
         for (int date = START_DATE; date <= calender.getLastDateOfMonth(); date++) {
-            String workerName = getWorkerName(calender, weekWorkers, weekendWorkers, date);
+            String workerName = getWorkerName(calender, orders, date);
             if (workedYesterday(date, workerName, workSchedule)) {
-                String nextTurnWorkerName = getWorkerName(calender, weekWorkers, weekendWorkers,
-                        date);
+                String nextTurnWorkerName = getWorkerName(calender, orders, date);
                 workSchedule.add(nextTurnWorkerName);
             }
             workSchedule.add(workerName);
@@ -30,12 +26,11 @@ public class AllocationService {
         return workSchedule.getWorker(date - 1).equals(workerName);
     }
 
-    private String getWorkerName(Calender calender, WorkingOrders weekWorkers,
-            WorkingOrders weekendWorkers, int date) {
+    private String getWorkerName(Calender calender, Orders orders, int date) {
         if (calender.isWeekend(date) || Holiday.isHoliday(calender.getMonth(), date)) {
-            return weekendWorkers.pullOutWorker();
+            return orders.getWeekendOrders().pullOutWorker();
         }
-        return weekWorkers.pullOutWorker();
+        return orders.getWeekOrders().pullOutWorker();
     }
 
 }
